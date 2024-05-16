4fce1759388a3e865a68dc316b71217469c67e3e
Eduardo <cilento762@gmail.com>
2024-05-16 17:30:13 -0300

    author dto, reference of posts by user, endpoint to return user posts, post by id

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.nelioalves.workshopmongo.dto.AuthorDto;
+
 @Document
 public class Post  implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -17,13 +19,13 @@ public class Post  implements Serializable{
 	private String title;
 	private String body;
 	
-	private User author;
+	private AuthorDto author;
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDto author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -64,11 +66,11 @@ public class Post  implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDto getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDto author) {
 		this.author = author;
 	}
 	
